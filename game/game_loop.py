386387ccf4387eb1202b83eb5b86e0b850ba4af2
Enoxboo <matteo.marquant@ynov.com>
2025-10-08 11:58:06 +0200

    feat(game_loop): add indications

--- game/game_loop.py
+++ game/game_loop.py
@@ -5,9 +5,12 @@ from models.player import Player
 def run_game():
     game_over = False
     days = 0
+    print("Tu te reveilles sur une île de prout et essaie de te souvenir de ton nom...")
     choice = input()
     player = Player(choice)
     while not game_over:
+        print(f"Jour : {days}")
+        print(f"Que faire aujourd'hui? ({' / '.join(player.actions.keys())})")
         choice = input()
         player.do_action(choice)
         days += 1
