f1e0ecc9cdd1577f3621048688d13da8cfe048ac
Enoxboo <matteo.marquant@ynov.com>
2025-10-08 11:35:37 +0200

    feat(game_loop): add simple game loop

--- game/game_loop.py
+++ game/game_loop.py
@@ -1,2 +1,16 @@
+from config import DAYS_TO_WIN
+from models.player import Player
+
+
 def run_game():
-    pass
+    game_over = False
+    days = 0
+    choice = input()
+    player = Player(choice)
+    while not game_over:
+        choice = input()
+        player.do_action(choice)
+        days += 1
+        if days == DAYS_TO_WIN:
+            print("You won.")
+            game_over = True
